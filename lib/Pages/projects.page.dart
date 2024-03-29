69ea0139f9134f73042a0d7d04ed79ee7111d562
MariemMimouni <mariemmimouni800@gmail.com>
2024-03-29 15:36:18 +0100

    Projects Page

--- lib/Pages/projects.page.dart
+++ lib/Pages/projects.page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sirati_app/Pages/awards.page.dart';
+import 'package:sirati_app/Pages/volunteer.page.dart';
+import 'package:sirati_app/Pages/work.page.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../Menu/drawer.widget.dart';
+import 'education.page.dart';
 class Projects extends StatelessWidget {
-  const Projects({super.key});
-
+final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +16,90 @@ class Projects extends StatelessWidget {
           elevation: 10,
           title: Text('سيرتي'),
         ),
-        body: Text('Projects')
+        body:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 400,
+                  child: PageView(
+                    controller: _controller,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Uri github = Uri.parse("https://github.com/MariemMimouni/PersonalPortfolio");
+                          launchUrl(github);                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              height: 200,
+                              color: Theme.of(context).colorScheme.onSurface,
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [Text('Personal Portfolio Website', style: TextStyle(color: Colors.white, fontSize:22, fontWeight: FontWeight.bold ),),
+                                    Text('School Project',style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic),),
+                                  Text('HTML - CSS - JavaScript',style: TextStyle(color: Colors.white),)]
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Uri github = Uri.parse("https://github.com/OnsKharrat06/FirstSkillsClub_Website");
+                          launchUrl(github);
+                          },
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              height: 200,
+                              color: Theme.of(context).colorScheme.onSurface,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [Text('First Skills Club Website', style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),),
+                                Text('Internship',style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic)),
+                                Text('HTML - CSS - JavaScript',style: TextStyle(color: Colors.white))]
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Uri github = Uri.parse("https://github.com/MariemMimouni/SiratiMobileApp");
+                          launchUrl(github);                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              height: 200,
+                              color: Theme.of(context).colorScheme.onSurface,
+                              child:Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [Text('SIRATI Mobile APP', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22),),
+                                    Text('School Project',style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic)),
+                                    Text('Flutter - Dart',style: TextStyle(color: Colors.white))]
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SmoothPageIndicator(controller: _controller,count: 3,
+                effect: ExpandingDotsEffect(
+                  activeDotColor: Theme.of(context).colorScheme.secondary,
+                  dotColor: Theme.of(context).colorScheme.onBackground,
+                  dotHeight: 30,
+                  dotWidth: 30,
+                ),)
+              ],
+            ),
     );
   }
 }
